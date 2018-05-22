345a658f950a9c4964cc94ae9d940e0783981de3
Julia <julettazhuk@gmail.com>
2018-05-22 11:44:19 +0700

    Name in different places in abuses, error with seconds fixed

--- Zhuk.Nsudotnet.NumberGuesser/Program.cs
+++ Zhuk.Nsudotnet.NumberGuesser/Program.cs
@@ -7,11 +7,11 @@ namespace NumberGuesser
         public static void Main(string[] args)
         {
             Random random = new Random();
-            string[] abuses = { ", I bet you JavaScript user . . .", ", is it legal to be as stuped as you are????",
-                ", oh God, you better play something easier.", ", you know, this program cheks your IQ, and I ahve baaaad news . . .",
-                ", at least, you tried.", ", are you sure you are human? I think you are dog at best. Good boy, try again.",
-                ", even I've tired already!!!", ", did you understand the rules?", ", hint: it's a number between 0 and 100 :3",
-                ", I wish something else lasts that long . . ."
+            string[] abuses = { "I bet, {0}, you JavaScript user . . .", "{0}, is it legal to be as stuped as you are????",
+                "Oh God, {0}, you better play something easier.", "You know, {0}, this program checks your IQ, and I ahve baaaad news . . .",
+                "At least, you tried, {0}.", "{0}, are you sure you are human? I think you are dog at best. Good boy, try again.",
+                "Even I've tired already, {0}!!!", "{0}, did you understand the rules?", "Hint for you, {0}: it's a number between 0 and 100 :3",
+                "Honey, {0}, I wish something else lasts that long . . . "
             };
             int[] history = new int[101]; // зачем нам 1000 элементов, ведь всего 100 чисел?
             Console.WriteLine("Hello, I'm Glados the Robot! Lets guess some numbers! Enter your name:");
@@ -27,7 +27,7 @@ namespace NumberGuesser
             DateTime start = DateTime.Now;
             while (puzzle != attempt)
             {
-                if (attemptNumber % 4 == 0) Console.WriteLine(name + abuses[random.Next(abuses.Length)]);
+                if (attemptNumber % 4 == 0) Console.WriteLine(abuses[random.Next(abuses.Length)], name);
                 string input = Console.ReadLine();
                 if (input == "q")
                 {
@@ -63,7 +63,7 @@ namespace NumberGuesser
             DateTime finish = DateTime.Now;
             TimeSpan totalTime = finish - start;
             Console.WriteLine("AT LAST! I thought it never ends!");
-            Console.WriteLine("You required for " + attemptNumber + " attempts and " + totalTime.Seconds + 
+            Console.WriteLine("You required for " + attemptNumber + " attempts and " + Math.Round(totalTime.TotalSeconds) + 
                               " seconds to guess simple number, shame on you!");
             Console.WriteLine("Your pathetic attempts:");
             for (int i = 1; i <= attemptNumber-1; i++)
